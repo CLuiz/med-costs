70a8ae55712f940de27d61928cff33384f3d8f7e
Chris Luiz <chris@chrisluiz.io>
2021-02-11 06:40:39 -0500

    function to get links to all Cleveland Clinic chargemaster docs

--- get_cleveland_clinic_data.py
+++ get_cleveland_clinic_data.py
@@ -8,8 +8,10 @@ from bs4.element import NavigableString
 
 CLINIC_URL = "https://my.clevelandclinic.org/patients/billing-finance/comprehensive-hospital-charges"
 
-def main():
 
+def get_links():
+    """ Get links to all Cleveland Clinic chargmaster docs.
+    """
     # Grab html
     soup = BeautifulSoup(requests.get(CLINIC_URL).text, "html5lib")
 
@@ -18,17 +20,30 @@ def main():
     # TODO Watch this id for changes!
     link_html = soup.find(id='101307de-b1e1-4693-9139-5fc9fec33baf').children
 
+
+    # Create dict of Hospital name, file url. The hospital names are present in the URLS, but I'd rather
+    # pull them out now than parse the urls later, as there are some inconsistencies.
+
+    link_dict ={x.text: x.attrs['href']
+            for x in link_html if type(x) != NavigableString}
+
+
     # Grab the url prefix from the clinic url to concat to each relative url
+
     prefix = CLINIC_URL.split('/patients')[0]
 
-    # Create tuples of Hospital name, file url. The hospital names are present in the URLS, but I'd rather
-    # pull them out now than parse the urls later, as there are some inconsistencies.
-    link_tuples =[(x.text, x.attrs['href'])
-            for x in link_html if type(x) != NavigableString]
+    # Add url prefix to the relative urls only
 
-    # TODO prefix needs to be added to the correct urls prior to return
-    return link_tuples
+    for k, v in link_dict.items():
+        if not v.startswith('https'):
+            link_dict[k] = ''.join([prefix, v])
+
+    return link_dict
+
+
+def main():
+    return get_links()
 
 
 if __name__ == '__main__':
-    link_tuples = main()
+    link_dict = main()
